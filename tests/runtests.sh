5ef1a73d92deb2b452936d6a3165596f9cffb298
Sterling DeMille <sterlingdemille+github@gmail.com>
2019-02-18 16:39:36 -0800

    Update tests dir

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -5,7 +5,9 @@ cd "$(dirname "$0")"
 python regtest.py -i "../target/debug/encrusted" czech.z3.regtest
 python regtest.py -i "../target/debug/encrusted" czech.z4.regtest
 python regtest.py -i "../target/debug/encrusted" czech.z5.regtest
-#python regtest.py -i "../target/debug/encrusted" praxix.z5.regtest
+python regtest.py -i "../target/debug/encrusted" czech.z8.regtest
+python regtest.py -i "../target/debug/encrusted" praxix.z5.regtest
 
 # Game tests
 python regtest.py -i "../target/debug/encrusted" curses.z3.regtest
+python regtest.py -i "../target/debug/encrusted" minizork.z3.regtest
